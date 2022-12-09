5d83280e4ff0db152bc1d6be6047f98a0cb062e7
huda-ayaz <hudaayaz21@gmail.com>
2022-12-09 14:43:17 -0500

    Commit all - updated home page and WIP ERG page

--- learnmore.js
+++ learnmore.js
@@ -2,25 +2,45 @@ const boxes = document.querySelectorAll(".box");
 const heading = document.querySelector(".heading span");
 const btn = document.querySelector(".btn");
 
-const colors = [
-  "#efd81d",
-  "#61dbfb",
-  "#41b883",
+const colorsText = [
+  "#fd8473",
+  "#e59999",// "#61dbfb",
+  "#a5531c",// "#df79d1",
   "#b52e31",
-  "#43853d",
+  "#8c5d53",// "#43853d",
   "#cf649a",
-  "#e04e39",
+  "#efcbc7",// "#e04e39",
+  //browns  "#7c4700",
+  // "#4b3a26",
+  // "#622a04",
+  // "#3A1F04",
+  // "#613613",
+  // "#351e10",
+  // "#402F1D",
 ];
 
-const techs = ["Diversirty", "Equity", "Inclusion", "Mission", "About", "ERG", "Value"];
+const colorsBtn = [
+  "#ffefd5",
+  "#e59999",// "#61dbfb",
+  "#a5531c",// "#df79d1",
+  "#b52e31",
+  "#8c5d53",// "#43853d",
+  "#cf649a",
+  "#efcbc7",// "#e04e39",
+];
+
+const techs = ["Click a box to learn more.", "Learn more about what we're doing for DEI.", "Learn more about what we're doing for DIVERSITY", "Learn more about what we're doing for EQUITY", "Learn more about what we're doing for INCLUSION", "Learn more about what we're doing for Employee Resourse Groups (ERGs)", "Learn more about what we're doing for MENTORSHIP"];
+const techsBtn = ["", "DEI", "Diversity", "Equity", "Inclusion", "ERGs", "Mentorship"];
+const techsBtnLink = ["#", "#DEI", "#Diversity", "#Inclusion", "#ERG", "#Mentorship"];
 
 let current = 1;
 
 const textStyle = () => {
-  heading.style.color = colors[current - 1];
+  heading.style.color = colorsText[current - 1];
   heading.textContent = techs[current - 1];
-  btn.style.backgroundColor = colors[current - 1];
-  btn.firstElementChild.textContent = techs[current - 1];
+  btn.style.backgroundColor = colorsBtn[current - 1];
+  btn.firstElementChild.textContent = techsBtn[current - 1];
+  // btn.href = techsBtnLink[current-1];
 };
 
 let interval = setInterval(() => {
