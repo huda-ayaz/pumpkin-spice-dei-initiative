42cef79e2b7b76f001cf55ae4fc6ca6a9462ee2d
huda-ayaz <hudaayaz21@gmail.com>
2022-12-11 12:06:47 -0500

    everything

--- learnmore.js
+++ learnmore.js
@@ -1,37 +1,31 @@
 const boxes = document.querySelectorAll(".box");
 const heading = document.querySelector(".heading span");
 const btn = document.querySelector(".btn");
+const btn__link = document.getElementById("btn__link");
 
 const colorsText = [
   "#fd8473",
-  "#e59999",// "#61dbfb",
-  "#a5531c",// "#df79d1",
+  "#e59999",
+  "#a5531c",
   "#b52e31",
-  "#8c5d53",// "#43853d",
+  "#efada7",
   "#cf649a",
-  "#efcbc7",// "#e04e39",
-  //browns  "#7c4700",
-  // "#4b3a26",
-  // "#622a04",
-  // "#3A1F04",
-  // "#613613",
-  // "#351e10",
-  // "#402F1D",
+  "#8c5d53"
 ];
 
 const colorsBtn = [
   "#ffefd5",
-  "#e59999",// "#61dbfb",
-  "#a5531c",// "#df79d1",
+  "#e59999",
+  "#a5531c",
   "#b52e31",
-  "#8c5d53",// "#43853d",
+  "#efada7",
   "#cf649a",
-  "#efcbc7",// "#e04e39",
+  "#8c5d53"
 ];
 
 const techs = ["Click a box to learn more.", "Learn more about what we're doing for DEI.", "Learn more about what we're doing for DIVERSITY", "Learn more about what we're doing for EQUITY", "Learn more about what we're doing for INCLUSION", "Learn more about what we're doing for Employee Resourse Groups (ERGs)", "Learn more about what we're doing for MENTORSHIP"];
 const techsBtn = ["", "DEI", "Diversity", "Equity", "Inclusion", "ERGs", "Mentorship"];
-const techsBtnLink = ["#", "#DEI", "#Diversity", "#Inclusion", "#ERG", "#Mentorship"];
+const techsBtnLink = ["#", "#learn__DEI", "#second", "#third", "#last", "erg.html", "dashboard.html"]; //dictionary
 
 let current = 1;
 
@@ -40,7 +34,7 @@ const textStyle = () => {
   heading.textContent = techs[current - 1];
   btn.style.backgroundColor = colorsBtn[current - 1];
   btn.firstElementChild.textContent = techsBtn[current - 1];
-  // btn.href = techsBtnLink[current-1];
+  btn__link.href = techsBtnLink[current - 1];
 };
 
 let interval = setInterval(() => {
